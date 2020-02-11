6d0b440ccfe0c341ef6959b4e1d49aa0b3b6b48d
Michael Kriese <michael.kriese@visualon.de>
2020-02-11 11:03:33 +0100

    feat: use git submodules (#4)

--- bin/get-docs.sh
+++ bin/get-docs.sh
@@ -8,28 +8,19 @@ docs=$pwd/docs
 rm -rf $docs
 mkdir -p $docs
 
-tmp=$pwd/tmp
-rm -rf $tmp
-mkdir -p $tmp
+deps=$pwd/deps
 
-cd $tmp
-git clone --depth=1 https://github.com/renovatebot/renovate
-cd renovate
-yarn install --ignore-optional  --frozen-lockfile
+cd $deps/renovate
+yarn install --frozen-lockfile
 yarn build
 cp -R docs/usage/* $docs
 yarn create-json-schema
 cp renovate-schema.json $docs
 
-cd $tmp
-git clone --depth=1 https://github.com/renovatebot/pro
-cd pro
+cd $deps/renovate-pro
 mkdir $docs/pro
 cp -R docs/* $docs/pro
 rm $docs/pro/README.md
-
-cd $tmp
-git clone --depth=1 https://github.com/renovatebot/renovate-config
 
 cd $pwd
 cp -R src/* docs/
