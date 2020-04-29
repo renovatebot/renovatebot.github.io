5dc35ad4c585335032dbf3c62fe51f63a5bfeb51
Rhys Arkins <rhys@arkins.net>
2020-04-29 16:11:02 +0200

    Remove Pro docs (#20)

--- bin/get-docs.sh
+++ bin/get-docs.sh
@@ -17,10 +17,5 @@ cp -R docs/usage/* $docs
 yarn create-json-schema
 cp renovate-schema.json $docs
 
-cd $deps/renovate-pro
-mkdir $docs/pro
-cp -R docs/* $docs/pro
-rm $docs/pro/README.md
-
 cd $pwd
 cp -R src/* docs/
