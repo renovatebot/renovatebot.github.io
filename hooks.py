da1329153172cceeacb8dd5ef18c60a39263aa67
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2022-02-21 09:21:48 +0100

    chore: add custom edit_uri code
    Code provided under public domain by Oleh Prypin.
    
    Thank you Oleh Prypin for creating this code.
    Offered code on GitHub: https://github.com/mkdocs/mkdocs/discussions/2757#discussioncomment-2092565
    Oleh Prypin's GitHub profile: https://github.com/oprypin

---
+++ hooks.py
@@ -0,0 +1,9 @@
+# This code is in the public domain.
+# Based on work from Oleh Prypin.
+# Link to their GitHub account: https://github.com/oprypin
+# Offered code as public domain: https://github.com/mkdocs/mkdocs/discussions/2757#discussioncomment-2092565
+
+def on_page_markdown(markdown, page, **kwargs):
+    edit_url = page.meta.get('edit_url')
+    if edit_url:
+        page.edit_url = edit_url
