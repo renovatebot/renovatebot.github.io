992578ecf494349ac6c47290ac4319666fa131de
Michael Kriese <michael.kriese@visualon.de>
2022-06-27 13:48:56 +0200

    Update bin/get-docs.sh

--- bin/get-docs.sh
+++ bin/get-docs.sh
@@ -22,8 +22,11 @@ cp -R tmp/docs/* "$docs"
 cd "$pwd"
 cp -R src/* docs/
 
-printf -- '---\n' > docs/merge-confidence.md
-printf -- 'title: Merge Confidence\ndescription: Node versions support in Renovate\n' >> docs/merge-confidence.md
-printf -- 'edit_url: https://github.com/whitesource/merge-confidence/edit/main/README.md\n' >> docs/merge-confidence.md
-printf -- '---\n\n' >> docs/merge-confidence.md
+{
+  printf -- '---\n'
+  printf -- 'title: Merge Confidence\ndescription: Node versions support in Renovate\n'
+  printf -- 'edit_url: https://github.com/whitesource/merge-confidence/edit/main/README.md\n'
+  printf -- '---\n\n'
+  curl -sSLf https://raw.githubusercontent.com/whitesource/merge-confidence/main/README.md
+} > docs/merge-confidence.md
 curl -sSLf https://raw.githubusercontent.com/whitesource/merge-confidence/main/README.md >> docs/merge-confidence.md
