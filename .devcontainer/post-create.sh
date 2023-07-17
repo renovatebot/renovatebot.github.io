20932bc4e191af65ec789496d64df9f22f43d179
Felipe Santos <felipecassiors@gmail.com>
2023-07-17 15:46:39 -0300

    chore: fix devcontainer on codespaces

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-set -ex
+set -e
+
+if [[ "${CODESPACES}" == true ]]; then
+  echo "Fixing permissions of /tmp for GitHub Codespaces..." >&2
+  sudo chmod 1777 /tmp
+fi
+
+set -x
 
 git config --global --add safe.directory "${PWD}"
 git config --global --add safe.directory "${PWD}/deps/renovate"
