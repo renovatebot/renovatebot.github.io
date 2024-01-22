562591c51721e076a8b66e024c5f2bd4fc315dad
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2024-01-22 12:31:04 +0100

    chore: fix typo in prepare-vercel script (#388)

--- bin/prepare-vercel.sh
+++ bin/prepare-vercel.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-PYTON_VERSION=3.12.1
+PYTHON_VERSION=3.12.1
 
 yum install -y \
   openssl11 \
@@ -12,14 +12,14 @@ yum install -y \
 
 mkdir -p /usr/local/python
 
-curl -sSLo /tmp/python.tar.xz "https://github.com/containerbase/python-prebuild/releases/download/${PYTON_VERSION}/python-amzn-2-x86_64.tar.xz"
+curl -sSLo /tmp/python.tar.xz "https://github.com/containerbase/python-prebuild/releases/download/${PYTHON_VERSION}/python-amzn-2-x86_64.tar.xz"
 
 tar -xf /tmp/python.tar.xz -C /usr/local/python
 
-ln -sf /usr/local/python/${PYTON_VERSION}/bin/python /usr/local/bin/python
-ln -sf /usr/local/python/${PYTON_VERSION}/bin/pip /usr/local/bin/pip
+ln -sf /usr/local/python/${PYTHON_VERSION}/bin/python /usr/local/bin/python
+ln -sf /usr/local/python/${PYTHON_VERSION}/bin/pip /usr/local/bin/pip
 
 python -m pip install poetry
-ln -sf /usr/local/python/${PYTON_VERSION}/bin/poetry /usr/local/bin/poetry
+ln -sf /usr/local/python/${PYTHON_VERSION}/bin/poetry /usr/local/bin/poetry
 
 poetry --version
