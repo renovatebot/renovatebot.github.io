d12b12b19542fdde7d75e60633d763bdf4720311
Michael Kriese <michael.kriese@visualon.de>
2024-03-19 11:49:01 +0100

    chore: drop python install

--- bin/prepare-vercel.sh
+++ bin/prepare-vercel.sh
@@ -2,24 +2,24 @@
 
 set -e
 
-PYTHON_VERSION=3.12.1
+# PYTHON_VERSION=3.12.1
 
-yum install -y \
-  openssl11 \
-  sqlite \
-  xz \
-  ;
+# yum install -y \
+#   openssl11 \
+#   sqlite \
+#   xz \
+#   ;
 
-mkdir -p /usr/local/python
+# mkdir -p /usr/local/python
 
-curl -sSLo /tmp/python.tar.xz "https://github.com/containerbase/python-prebuild/releases/download/${PYTHON_VERSION}/python-amzn-2-x86_64.tar.xz"
+# curl -sSLo /tmp/python.tar.xz "https://github.com/containerbase/python-prebuild/releases/download/${PYTHON_VERSION}/python-amzn-2-x86_64.tar.xz"
 
-tar -xf /tmp/python.tar.xz -C /usr/local/python
+# tar -xf /tmp/python.tar.xz -C /usr/local/python
 
-ln -sf /usr/local/python/${PYTHON_VERSION}/bin/python /usr/local/bin/python
-ln -sf /usr/local/python/${PYTHON_VERSION}/bin/pip /usr/local/bin/pip
+# ln -sf /usr/local/python/${PYTHON_VERSION}/bin/python /usr/local/bin/python
+# ln -sf /usr/local/python/${PYTHON_VERSION}/bin/pip /usr/local/bin/pip
 
 python -m pip install poetry
-ln -sf /usr/local/python/${PYTHON_VERSION}/bin/poetry /usr/local/bin/poetry
+# ln -sf /usr/local/python/${PYTHON_VERSION}/bin/poetry /usr/local/bin/poetry
 
 poetry --version
