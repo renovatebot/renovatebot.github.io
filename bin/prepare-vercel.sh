7272deb30377fe4144180d741510d71be0e96494
Michael Kriese <michael.kriese@visualon.de>
2024-03-19 11:59:22 +0100

    pind poetry version and use python3

--- bin/prepare-vercel.sh
+++ bin/prepare-vercel.sh
@@ -2,24 +2,9 @@
 
 set -e
 
-# PYTHON_VERSION=3.12.1
+# renovate: datasource=pypi depName=poetry
+POETRY_VERSION=1.8.2
 
-# yum install -y \
-#   openssl11 \
-#   sqlite \
-#   xz \
-#   ;
-
-# mkdir -p /usr/local/python
-
-# curl -sSLo /tmp/python.tar.xz "https://github.com/containerbase/python-prebuild/releases/download/${PYTHON_VERSION}/python-amzn-2-x86_64.tar.xz"
-
-# tar -xf /tmp/python.tar.xz -C /usr/local/python
-
-# ln -sf /usr/local/python/${PYTHON_VERSION}/bin/python /usr/local/bin/python
-# ln -sf /usr/local/python/${PYTHON_VERSION}/bin/pip /usr/local/bin/pip
-
-python -m pip install poetry
-# ln -sf /usr/local/python/${PYTHON_VERSION}/bin/poetry /usr/local/bin/poetry
+python3 -m pip install poetry==${POETRY_VERSION}
 
 poetry --version
