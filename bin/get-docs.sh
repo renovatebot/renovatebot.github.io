aef28d3b5e5e3b65a929b60999f73ec79e4082c1
Michael Kriese <michael.kriese@visualon.de>
2025-02-05 15:37:55 +0100

    build: use pnpm instead of corepack (#623)

--- bin/get-docs.sh
+++ bin/get-docs.sh
@@ -13,8 +13,9 @@ deps=$pwd/deps
 cd "$deps/renovate"
 
 if [ -z "$SKIP_BUILD" ]; then
-  corepack pnpm install --frozen-lockfile
-  corepack pnpm build:docs
+ # fix version mismatch until we're on pnpm v10
+  COREPACK_ENABLE_STRICT=0 pnpm install --frozen-lockfile
+  COREPACK_ENABLE_STRICT=0 pnpm build:docs
 fi
 
 cp -R tmp/docs/. "$docs"
