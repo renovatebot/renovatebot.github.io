f6f5b00210dbaca94a8f0545bb6df18d676e635f
Michael Kriese <michael.kriese@visualon.de>
2025-02-05 15:44:03 +0100

    chore: testing

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -8,4 +8,11 @@ if [[ "${CODESPACES}" == true ]]; then
 fi
 
 git submodule update --init
-make STRICT=false
+# make STRICT=false
+
+ls -la /tmp/
+ls -la /home/vscode/
+ls -la /home/vscode/.local/
+ls -la /home/vscode/.local/share/
+ls -la /home/vscode/.local/share/pdm/
+pdm install
